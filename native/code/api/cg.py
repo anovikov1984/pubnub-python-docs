12fc5137efafaf4ee7cbc12331a1e16b5a5f44ba
Alexander Novikov <alexander@pubnub.com>
2016-09-27 09:08:34 -0700

    Working on native api

---
+++ native/code/api/cg.py
@@ -0,0 +1,35 @@
+import logging
+import pubnub
+from pubnub.callbacks import SubscribeCallback
+from pubnub.enums import PNOperationType, PNStatusCategory
+
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.pubnub import PubNub, SubscribeListener
+
+pubnub.set_stream_logger('pubnub', logging.DEBUG)
+
+pnconfig = PNConfiguration()
+
+pnconfig.publish_key = "pub-c-98863562-19a6-4760-bf0b-d537d1f5c582"
+pnconfig.subscribe_key = "sub-c-7ba2ac4c-4836-11e6-85a4-0619f8945a4f"
+pnconfig.secret_key = "sec-c-MGFkMjQxYjMtNTUxZC00YzE3LWFiZGYtNzUwMjdjNmM3NDhk"
+
+pubnub = PubNub(pnconfig)
+
+
+def grant_handler(result, status):
+    if status.is_error():
+        # handle error
+        return
+
+    print(result)
+
+
+pubnub.add_channel_to_channel_group()
+
+
+
+
+
+
+
